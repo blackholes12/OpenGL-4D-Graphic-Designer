3a341c8e646e9d5348375f36d9929f50837e6bf2
blackholes12 <xy13426685712@outlook.com>
2024-03-02 10:28:02 +0800

    Add the function of deleting connectors and objects with delete key, added highlighted areas for objects to be displayed during area deletion and fix the issue of flickering during spring construction

---
+++ OpenGL4d_20240302_114830/OpenGL4d/OpenGL2/main.cpp
@@ -0,0 +1,37 @@
+#include "Game.h"
+
+//ZOOOOOOOOOOOM IN BEFORE RECORDING!
+//static void cursor_position_callback(GLFWwindow* window, double positionX, double positionY);
+//void cursor_enter_callback(GLFWwindow* window, int entered);
+//void mouse_button_callback(GLFWwindow* window, int button,int action, int mods);
+void framebuffer_resize_callback(GLFWwindow* window, int fbW, int fbH);
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset);
+Game* game;
+int main(){
+	game=new Game("YOUTUBE_TUTORIAL",1920, 1080,4, 4,true);
+	//glfwSetCursorPosCallback(game->window, cursor_position_callback);
+	//glfwSetCursorEnterCallback(game->window, cursor_enter_callback);
+	//glfwSetMouseButtonCallback(game->window, mouse_button_callback);
+	glfwSetFramebufferSizeCallback(game->window, framebuffer_resize_callback);
+	glfwSetScrollCallback(game->window, scroll_callback);
+	//MAIN LOOP
+	while (!game->getWindowShouldClose())
+	{
+		//UPDATE INPUT ---
+		game->update0();
+		if(game->isCloseUI)
+			glfwSetScrollCallback(game->window, scroll_callback);
+		game->render0();
+	}
+	delete game;
+	return 0;
+}
+void framebuffer_resize_callback(GLFWwindow* window, int fbW, int fbH){
+	glViewport(0, 0, fbW, fbH);
+	game->updateProjectMat();
+	//std::cout << "fbH=" << fbH << "\n";
+};
+
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset){
+	game->updateModes(-1, yOffset);
+};
